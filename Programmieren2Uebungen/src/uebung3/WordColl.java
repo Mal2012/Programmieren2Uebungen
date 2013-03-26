890db1a3df0eed97b08a20c855ca4f47171860c2
Mal2012 <malcolmkoegler+github@gmail.com>
2013-03-26 10:47:35 +0100

    WordCollection ohne Verwendung von Vector

---
+++ Programmieren2Uebungen/src/uebung3/WordColl.java
@@ -0,0 +1,87 @@
+package uebung3;
+
+import java.util.StringTokenizer;
+
+public class WordColl {
+	String[] collection;
+
+	public WordColl(String... chapter) {
+		this.collection = chapter;
+	}
+
+	public int size() {
+		int result = 0;
+		for (int i = 0; i < this.collection.length; i++) {
+			StringTokenizer st = new StringTokenizer(collection[i]);
+			while (st.hasMoreTokens()) {
+				st.nextToken();
+				result += 1;
+			}
+		}
+
+		return result;
+
+	}
+
+	public int count(String s) {
+		int result = 0;
+		for (int i = 0; i < this.collection.length; i++) {
+			StringTokenizer st = new StringTokenizer(collection[i]);
+			while (st.hasMoreTokens()) {
+				if (st.nextToken().equals(s)) {
+
+					result += 1;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public void append(String... chapter) {
+
+		String[] temp = this.collection;
+		this.collection = new String[temp.length + chapter.length];
+		System.arraycopy(temp, 0, this.collection, 0, temp.length);
+		System.arraycopy(chapter, 0, this.collection, temp.length,
+				chapter.length);
+	}
+
+	public String top() {
+		int c = 0;
+		String res = null;
+		for (int i = 0; i < this.collection.length; i++) {
+			StringTokenizer st = new StringTokenizer(this.collection[i]);
+			while (st.hasMoreTokens()) {
+				String string = st.nextToken();
+				if (count(string) > c) {
+					c = count(string);
+					res = string;
+				}
+			}
+
+		}
+
+		return res;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		int total = 0;
+		for (int i = 0; i < this.collection.length; i++) {
+			StringTokenizer st = new StringTokenizer(this.collection[i]);
+
+			while (st.hasMoreTokens()) {
+				String s = st.nextToken();
+				sb.append(s + "				| " + count(s) + " \n");
+				total += count(s);
+			}
+		}
+		return "Word				| count\n"
+				+ "------------------------------------------------\n"
+				+ sb.toString()
+				+ "------------------------------------------------\n"
+				+ "Total: " + total;
+	}
+}
---
+++ Programmieren2Uebungen/src/uebung3/WordCollTest.java
@@ -0,0 +1,19 @@
+package uebung3;
+
+public class WordCollTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] chapter = { "Thomas Mann , der jüngere",
+				"Bruder von Heinrich Mann" };
+
+		WordColl wColl = new WordColl(chapter);
+		System.out.println(wColl.size()); // liefert 9.
+		System.out.println(wColl.count("Mann"));
+		System.out.println(wColl);
+		System.out.println(wColl.top());
+	}
+}
