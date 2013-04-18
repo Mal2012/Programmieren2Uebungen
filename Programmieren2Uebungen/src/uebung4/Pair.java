06915d11fc08f70be747fb1fe92dd0bb3012c106
Mal2012 <malcolmkoegler+github@gmail.com>
2013-04-18 21:43:47 +0200

    fhg

---
+++ Programmieren2Uebungen/src/uebung4/Pair.java
@@ -0,0 +1,54 @@
+package uebung4;
+
+public class Pair<E, K> {
+	private E e;
+	private K k;
+
+	public Pair(E ee, K kk) {
+		this.e = ee;
+		this.k = kk;
+	}
+
+	public E getE() {
+		return e;
+	}
+
+	public void setE(E e) {
+		this.e = e;
+	}
+
+	public K getK() {
+		return k;
+	}
+
+	public void setK(K k) {
+		this.k = k;
+	}
+
+	@Override
+	public String toString() {
+		return "Pair [e=" + e + ", k=" + k + "]";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null)
+			return false;
+		if (getClass() != o.getClass())
+			return false;
+		if (!super.equals(o))
+			return false;
+		final Pair other = (Pair) o;
+		if (!helpEquals(e, other.getE()))
+			return false;
+		if (!helpEquals(k, other.getK()))
+			return false;
+		return true;
+	}
+
+	private static boolean helpEquals(final Object x, final Object y) {
+		if (x == null)
+			return y == null;
+		return x.equals(y);
+	}
+}
